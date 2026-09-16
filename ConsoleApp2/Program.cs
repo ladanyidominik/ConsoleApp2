40c2971e9232309387cd1dbe454190cbb9d98598
ladanyidominik <ladanyi.dominik0820@gmail.com>
2026-09-16 12:19:02 +0200

    main

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Sziasztok köcsögök");
+            Console.WriteLine("asdasdasd");
         }
     }
 }
