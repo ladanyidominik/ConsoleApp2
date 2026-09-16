c1e3f3afd646c066a49b73be7fffa1beffdbb074
ladanyidominik <ladanyi.dominik0820@gmail.com>
2026-09-16 12:18:14 +0200

    after save

--- ConsoleApp2/NewClass.cs
+++ ConsoleApp2/NewClass.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
-    internal class NewClass
+    public class NewClass
     {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
     }
 }
